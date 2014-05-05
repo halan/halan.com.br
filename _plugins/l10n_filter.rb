c664e64f493d3d9268174d46aeb5d1e7cb0163fd
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-05-04 21:30:09 -0300

    Avoid l10n bug when watch files

--- _plugins/l10n_filter.rb
+++ _plugins/l10n_filter.rb
@@ -1,19 +1,14 @@
 require 'i18n'
 
+I18n.enforce_available_locales = false
+I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
+
 module Jekyll
   module L10nFilter
     def localize(input, format=nil)
-      load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
+      I18n.locale = Jekyll.configuration({})['locale']
       I18n.l input, :format => format
-    end
-
-    def load_translations
-      unless I18n::backend.instance_variable_get(:@translations)
-        I18n.enforce_available_locales = false
-        I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
-        I18n.locale = Jekyll.configuration({})['locale']
-      end
     end
   end
 end
